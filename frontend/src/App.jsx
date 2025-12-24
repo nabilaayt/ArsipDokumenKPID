bd85d3d0265b54fda5159b9d275f186b06568cb6
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-12-24 11:42:37 +0700

    feat: for be add cors fe and fe add protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,105 @@
 import { Routes, Route } from "react-router-dom";
+import { createContext } from "react";
+import { Toaster } from "react-hot-toast";
+
+// Pages Public
 import Login from "./pages/Login";
+import KonversiFile from "./pages/KonversiFile";
+
+// Pages Admin
 import AdminDashboard from "./pages/admin/Dashboard";
 import AdminDokumen from "./pages/admin/Dokumen";
-import KonversiFile from "./pages/KonversiFile";
 import AddDokumen from "./pages/admin/AddDokumen";
+
+// Pages User
 import DashboardUser from "./pages/admin/Dashboard";
 import DokumenUser from "./pages/user/DokumenUser";
 import EditDokumen from "./pages/admin/EditDokumen";
 
+// Components
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
 function App() {
 
   return (
-    <Routes>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
 
-      {/* Public Routes */}
-      <Route path="/" element={<Login />}/>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />}/>
 
-      {/* Admin Routes */}
-      <Route path="/admin/dashboard" element={<AdminDashboard />}/>
-      <Route path="/admin/dokumen" element={<AdminDokumen />}/>
-      <Route path="/admin/tambahDokumen" element={<AddDokumen />}/>
-      <Route path="/admin/editDokumen" element={<EditDokumen />}/>
-      <Route path="/admin/konversiFile" element={<KonversiFile />} />
+        {/* Admin Routes */}
+        <Route 
+          path="/admin/dashboard" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/admin/dokumen" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <AdminDokumen />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/admin/tambahDokumen" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <AddDokumen />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/admin/editDokumen" 
+          element={
+          <ProtectedRoute allowedRole="admin">
+              <EditDokumen />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/admin/konversiFile" 
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <KonversiFile />
+            </ProtectedRoute>
+          } 
+        />
 
-      {/* User Routes */}
-      <Route path="/user/dashboard" element={<DashboardUser />}/>
-      <Route path="/user/dokumen" element={<DokumenUser />}/>
-      <Route path="/user/konversiFile" element={<KonversiFile />} />
+        {/* User Routes */}
+        <Route 
+          path="/user/dashboard" 
+          element={
+            <ProtectedRoute allowedRole="user">
+              <DashboardUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/user/dokumen" 
+          element={
+            <ProtectedRoute allowedRole="user">
+              <DokumenUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/user/konversiFile" 
+          element={
+            <ProtectedRoute allowedRole="user">
+              <KonversiFile />
+            </ProtectedRoute>
+          } 
+        />
 
-    </Routes>
+      </Routes>
+    </>
   );
 };
 
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+export default function ProtectedRoute({ children, allowedRole }) {
+    const { user, isAuth } = useAuth();
+
+    // Belum login
+    if(!isAuth || !user) {
+        return <Navigate to="/" replace />
+    }
+
+    // Jika role berbeda maka, arahkan sesuai role nya
+    if(allowedRole && user.role !== allowedRole) {
+        if(user.role === "admin") return <Navigate to="/admin/dashboard" replace />;
+        if(user.role === "user") return <Navigate to="/user/dashboard" replace />;
+        return <Navigate to="/" replace />;
+    }
+
+    return children;
+};
