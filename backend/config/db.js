a4cfcd062d70712026653ada989b0ff90fa76146
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2026-01-13 05:28:05 +0700

    fix: config db backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,21 @@
 const mysql = require('mysql2');
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'kpid_sumsel',
+    // host: process.env.DB_HOST || 'localhost',
+    // user: process.env.DB_USER || 'root',
+    // password: process.env.DB_PASS || '',
+    // database: process.env.DB_NAME || 'kpid_sumsel',
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
 });
 
 db.getConnection((err, connection) => {
